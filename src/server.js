a0bb0e8179fe7ebe237af68e3f5c8261e174603d
MinChan Lee <shigatsu970704@gmail.com>
2021-11-12 15:19:54 +0900

    Add Face API on Seowon Project

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import http from "http";
 import express from "express";
-import SocketIO from "socket.io";
-
+import http from "http";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 
 const app = express();
 
@@ -14,73 +14,82 @@ app.get("/*", (_, res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+
+instrument(wsServer, {
+  auth: false,
+});
 
 function publicRooms() {
-    const {
-        sockets: {
-          adapter: { sids, rooms },
-        },
-    } = wsServer;
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if (sids.get(key) === undefined) {
-            publicRooms.push(key);
-        }
-    });
-    return publicRooms;
+  const {
+    sockets: {
+      adapter: { sids, rooms },
+    },
+  } = wsServer;
+  const publicRooms = [];
+  rooms.forEach((_, key) => {
+    if (sids.get(key) === undefined) {
+      publicRooms.push(key);
+    }
+  });
+  return publicRooms;
 }
-    
+
 function countRoom(roomName) {
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
+  return wsServer.sockets.adapter.rooms.get(roomName)?.size;
 }
 
 wsServer.on("connection", (socket) => {
-    socket.onAny((event) => {
-      console.log(`Socket Event: ${event}`);
+  socket.onAny((event) => {
+    console.log(`Socket Event: ${event}`);
+  });
+
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+
+  socket.on("get_room", () => {
+    socket.emit("get_room", publicRooms());
+  });
+
+  socket.on("enter_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("enter_room");
+    wsServer
+      .to(roomName)
+      .emit("enter_room_all", socket.nickname, countRoom(roomName));
+    wsServer.sockets.emit("room_change", publicRooms());
+  });
+
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+
+  socket.on("new_message", (message, room, done) => {
+    socket.to(room).emit("new_message", `${socket.nickname}: ${message}`);
+    done();
+  });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) => {
+      socket.to(room).emit("leave_room", socket.nickname, countRoom(room) - 1);
     });
-  
-    socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
-  
-    socket.on("get_room", () => {
-      socket.emit("get_room", publicRooms());
-    });
-  
-    socket.on("enter_room", (roomName) => {
-      socket.join(roomName);
-      socket.to(roomName).emit("enter_room");
-      wsServer
-        .to(roomName)
-        .emit("enter_room_all", socket.nickname, countRoom(roomName));
-      wsServer.sockets.emit("room_change", publicRooms());
-    });
-  
-    socket.on("offer", (offer, roomName) => {
-      socket.to(roomName).emit("offer", offer);
-    });
-  
-    socket.on("answer", (answer, roomName) => {
-      socket.to(roomName).emit("answer", answer);
-    });
-  
-    socket.on("ice", (ice, roomName) => {
-      socket.to(roomName).emit("ice", ice);
-    });
-  
-    socket.on("new_message", (message, room, done) => {
-      socket.to(room).emit("new_message", `${socket.nickname}: ${message}`);
-      done();
-    });
-  
-    socket.on("disconnecting", () => {
-      socket.rooms.forEach((room) => {
-        socket.to(room).emit("leave_room", socket.nickname, countRoom(room) - 1);
-      });
-    });
-  
-    socket.on("disconnect", () => {
-      wsServer.sockets.emit("room_change", publicRooms());
-    });
+  });
+
+  socket.on("disconnect", () => {
+    wsServer.sockets.emit("room_change", publicRooms());
+  });
 });
-  
+
 httpServer.listen(3000, handleListen);
